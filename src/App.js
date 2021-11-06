1de60de59f2a1bac1acd6c936e2f22915e64191b
Javier Sanchez <javier.san.mail@gmail.com>
2021-11-06 16:39:02 +0100

    implemented haar cascade detection algorithm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './App.css';
 
 const App = () => {
   const videoRef = useRef(null);
+  const outputCanvas = useRef(null);
+  const detectionCanvas = useRef(null);
+  const [faceClassifier, setFaceClassifier] = useState(null);
+  const [srcMat, setSRCMat] = useState(null);
+  const [grayMat, setGrayMat] = useState(null);
+  const [detectionCanvasCtx, setDetectionCanvasCtx] = useState(null);
+  const [outputCanvasCtx, setOutputCanvasCtx] = useState(null);
 
     // LOAD OPENCV
   useEffect(() => {
@@ -60,14 +67,84 @@ const App = () => {
     };
     try {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      //setStreams(stream);
       videoRef.current.srcObject = stream;
       await videoRef.current.play();
       console.log("Video PLAYED!")
+      startVideoProcessing()
     } catch {
-      console.log("WWW")
+      console.log("ERRROR CAMERA")
     }
   };
+
+
+  function startVideoProcessing() {
+    detectionCanvas.current.width = videoRef.current.videoWidth;
+    detectionCanvas.current.height = videoRef.current.videoHeight;
+    outputCanvas.current.width = videoRef.current.videoWidth;
+    outputCanvas.current.height = videoRef.current.videoHeight;
+    
+
+    setDetectionCanvasCtx(detectionCanvas.current.getContext('2d'));
+    setOutputCanvasCtx(outputCanvas.current.getContext('2d'));
+    
+    setSRCMat(new window.cv.Mat(videoRef.current.videoHeight, videoRef.current.videoWidth, window.cv.CV_8UC4));
+    setGrayMat(new window.cv.Mat(videoRef.current.videoHeight, videoRef.current.videoWidth, window.cv.CV_8UC1));
+
+    setFaceClassifier(new window.cv.CascadeClassifier());
+
+
+    
+    //processVideo();
+  }
+
+  useEffect(() => {
+    if ( faceClassifier) {
+      try {
+        faceClassifier.load('haarcascade_frontalface_default.xml');
+        processVideo();
+      }catch (e) {
+        console.log("ERROR LOADING MODEL", e)
+      }
+    }
+  }, [faceClassifier])
+
+  function drawResults(ctx, results, color, size) {
+    for (let i = 0; i < results.length; ++i) {
+      let rect = results[i];
+      let xRatio = videoRef.current.videoWidth/size.width;
+      let yRatio = videoRef.current.videoHeight/size.height;
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = color;
+      ctx.strokeRect(rect.x*xRatio, rect.y*yRatio, rect.width*xRatio, rect.height*yRatio);
+    }
+  }
+
+
+
+  function processVideo() {
+    //Paint frame to canvas
+    outputCanvasCtx.clearRect(0, 0, outputCanvas.current.width, outputCanvas.current.height);
+    detectionCanvasCtx.drawImage(videoRef.current, 0, 0, videoRef.current.videoWidth, videoRef.current.videoHeight);
+    let imageData = detectionCanvasCtx.getImageData(0, 0, videoRef.current.videoWidth, videoRef.current.videoHeight);
+    srcMat.data.set(imageData.data);
+    window.cv.cvtColor(srcMat, grayMat, window.cv.COLOR_RGBA2GRAY);
+    let faces = [];
+    let size;
+    let faceVect = new window.cv.RectVector();
+    let faceMat = new window.cv.Mat();
+    window.cv.pyrDown(grayMat, faceMat);
+    window.cv.pyrDown(faceMat, faceMat);
+    size = faceMat.size();
+    faceClassifier.detectMultiScale(faceMat, faceVect);
+    for (let i = 0; i < faceVect.size(); i++) {
+      let face = faceVect.get(i);
+      faces.push(new window.cv.Rect(face.x, face.y, face.width, face.height));
+    }
+    faceMat.delete();
+    faceVect.delete();
+    drawResults(outputCanvasCtx, faces, 'red', size);
+    requestAnimationFrame(processVideo);
+  }
 
   return (
     <div className="App">
@@ -75,6 +152,8 @@ const App = () => {
           ref={videoRef}
           className="videoCamera"
         />
+        <canvas className="videoOutput" ref={outputCanvas}></canvas>
+        <canvas className="videoCamera detectionCanvas" ref={detectionCanvas}></canvas>
     </div>
   );
 };
